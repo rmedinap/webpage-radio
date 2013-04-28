d2cb36d943c1ce1f016572f30317ff25eb307f9b
Rafael Medina <rmedinap@gmail.com>
2013-04-27 19:41:43 -0500

    Hecho el player popup

--- app/controllers/radio_online_controller.rb
+++ app/controllers/radio_online_controller.rb
@@ -1,4 +1,9 @@
 class RadioOnlineController < ApplicationController
   def index
+    @auspiciadores = Auspiciador.all
+
+    respond_to do |format|
+      format.html { render layout: 'layout_radio'}  
+    end
   end
 end
