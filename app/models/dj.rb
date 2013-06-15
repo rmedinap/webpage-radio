b3379c69ae52c7796d425c259915f5db3b4ff383
Paul Sutcliffe <paul@kosmyka.com>
2013-06-15 15:30:10 -0500

    Added link to dj model

--- app/models/dj.rb
+++ app/models/dj.rb
@@ -1,7 +1,7 @@
 class Dj < ActiveRecord::Base
-  attr_accessible :nombre, :imagen_dj
+  attr_accessible :nombre, :imagen_dj, :link
 
   has_attached_file :imagen_dj, styles: {
     index: {geometry: '190x190'}
-  }  
+  }
 end
