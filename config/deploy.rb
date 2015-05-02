a369ee7382c0ab3eafd261c93fca2e350905da96
Rafael Medina <rmedinap@gmail.com>
2015-05-02 09:24:47 -0500

    updated repo

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,7 @@ set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"
-set :repository, "git@github.com:paulsutcliffe/#{application}.git"
+set :repository, "git@github.com:rmedinap/#{application}.git"
 set :branch, "master"
 
 default_run_options[:pty] = true
